8027ff4f9d51cee47a0aab478448f098062a98ba
06Solec <nmrozinska@2slo.pl>
2026-04-24 13:44:28 +0200

    test

---
+++ MyGame.py
@@ -0,0 +1,177 @@
+import pygame as py
+from random import randint
+from Playing import Player, Obstacle
+py.mixer.init()
+#stone img: https://publicdomainvectors.org/en/free-clipart/Pile-of-rocks/74332.html
+#bg img : https://media.easy-peasy.ai/92095ba3-b100-486f-81bb-376973e32d8c/d204c906-1a2b-4078-8cc5-c5ed74cf48af.png
+#in this script we will generate n x n grid on the screen
+#our player can only move within these cells in the grid
+cell_w, cell_h = 60, 60
+row, col = 9, 9
+screen_w, screen_h = col * cell_w, row*cell_h
+panel_w = 3*cell_w
+sixes = 0
+
+#generating ranodm value of 0 and 1 in the grid list
+#which decides where we draw obstacles
+#we choose our probabilities to be 80 - 20 against obstacle.
+clock = py.time.Clock()
+py.init()
+screen = py.display.set_mode((screen_w + panel_w,screen_h))
+py.display.set_caption("Generating random grid")
+
+char = py.image.load("C:\\CSprojectDungeonCrawler\\char.svg")
+char = py.transform.scale(char, (60, 60))
+stones = py.image.load("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\stones.svg")
+stones = py.transform.scale(stones, (60, 60))
+bg = py.image.load("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\bg.png")
+bg = py.transform.scale(bg, (screen_w, screen_h))
+gems = py.image.load("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\Crystal.svg")
+gems = py.transform.scale(gems, (60, 60))
+portal = py.image.load("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\Orange-Gem.svg")
+portal = py.transform.scale(portal, (60, 60))
+dig = py.mixer.Sound("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\Firework_blast.ogg")
+gem_sound = py.mixer.Sound("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Gra\\CSprojectDungeonCrawler\\Armor_Stand_break1.ogg")
+
+p1 = Player(0, 0, 60, 60, char)
+
+def gridChange():
+    grid = [[randint(0, 4) for _ in range(col)] for _ in range(row)]
+
+    # ensure starting area is free
+    grid[0][0], grid[0][1], grid[1][0] = 1, 1, 1
+
+    obstacleList = []
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c] == 0:
+                obstacleList.append(Obstacle(c * cell_w, r * cell_h, stones))
+            elif grid[r][c] == 3:
+                grid[r][c] = 6
+
+    return grid, obstacleList
+
+grid, obstacleList = gridChange()
+
+
+for r in range(row):
+    for c in range(col):
+        if grid[r][c] == 0:
+            obstacleList.append(Obstacle(c*cell_w, r*cell_h, stones))#we append the obstacles based on grid
+        elif grid[r][c] == 3:
+            grid[r][c] = 6
+            
+def sixIs(sixes):
+    sixes = 0
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c] == 6:
+                sixes += 1
+    return sixes
+'''
+coinList = []
+for r in range(row):
+    for c in range(col):
+        if grid[r][c] == 2:
+            coinList.append(Coin(c*cell_w, r*cell_h, money))
+'''
+
+#set the speedX and speedY to the cell width and cell height so that
+#the player moves exactly as the amount of a cell
+#Player.speedX = cell_w
+#Player.speedY = cell_h
+# p2 = Player(500, 500, 50, 50)
+
+
+
+def drawGrid(grid:list[list], obstacleList):
+    index_obstacle = 0
+    #index_coin = 0
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c] == 0:
+                # py.draw.rect(screen,"#000000", (cell_w*c, cell_h*r, cell_w, cell_h))
+                obstacleList[index_obstacle].draw(screen)
+                index_obstacle += 1
+            
+            elif grid[r][c] == 6:
+                screen.blit(gems, (c*cell_w, r*cell_h))
+
+            elif grid[r][c] == 5:
+                screen.blit(portal, (c*cell_w, r*cell_h))
+            
+            '''
+            if grid[r][c] == 2:
+                coinList[index_coin].draw(screen)
+                index_coin += 1
+            '''
+            
+gem = 0
+portal_passes = 0
+def draw_panel(screen, gem, portal_passes):
+    font = py.font.SysFont(None, 30)
+    py.draw.rect(screen, "#333333", (screen_w, 0, panel_w, screen_h))
+    textSurface1 = font.render(f"Coins: {gem}", True, "#cccccc")
+    textSurface2 = font.render(f"Portals: {portal_passes}", True, "#cccccc")
+    screen.blit(textSurface1, (screen_w + 20, 40))
+    screen.blit(textSurface2, (screen_w + 20, 65))
+
+def find(gem):
+    r = p1.y // 60
+    c = p1.x // 60
+    if event.type == py.KEYDOWN:
+        if event.key == py.K_SPACE and grid[r][c] == 6:
+            gem += 1
+            grid[r][c] = 3
+            gem_sound.play()
+    return gem
+
+
+
+def check(sixes):
+    if sixes == 0:
+        grid[0][0] = 5
+
+
+def portalCheck(portal_passes, grid, obstacleList):
+    r = p1.y // cell_h
+    c = p1.x // cell_w
+
+    if event.type == py.KEYDOWN:
+        if event.key == py.K_SPACE and grid[r][c] == 5:
+            grid, obstacleList = gridChange()
+            portal_passes += 1
+
+    return portal_passes, grid, obstacleList
+
+            
+            
+run = True
+while run:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            run = False
+        p1.move(screen, grid, event)
+        gem = find(gem)
+        sixes = sixIs(sixes)
+        check(sixes)
+        portal_passes, grid, obstacleList = portalCheck(portal_passes, grid, obstacleList)
+        grid = p1.mine(screen, grid, event)
+    #we will need to change the frame rate for this program
+    clock.tick(15)
+    #clean the previous history of the screen
+    # screen.fill("#ffffff")
+    screen.blit(bg, (0,0))
+    #first draw the grid
+    drawGrid(grid, obstacleList)
+    draw_panel(screen, gem, portal_passes)
+    #then draw the player
+    p1.draw(screen)
+    #then move the player
+    #p1.move(screen)
+    #for enemy in obstacleList:
+        #p1.collision(enemy)
+
+    #update the sreen
+    py.display.flip()
+py.quit()
---
+++ Playing.py
@@ -0,0 +1,130 @@
+import pygame as py
+from random import randint
+py.mixer.init()
+
+class Player:
+    '''
+    Player is a rectangle object of pygame
+    So it must take x, y, width and height
+    '''
+    #the following variables are known as static or class variables
+    speedX, speedY = randint(3, 6), randint(1, 4)
+    dig = py.mixer.Sound("C:\\Users\\06Solec\\Documents\\NataliaM\\GameProject\\kodPana\\Firework_blast.ogg")
+    # bark = py.mixer.Sound()
+
+
+    def __init__(self, x:int, y:int, w:int, h:int, img):
+        #the following variables are called instance variables
+        #because they are specific to a given object
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.rect = (self.x, self.y, self.w, self.h)
+        self.collide = False
+        self.img = img
+    
+    def draw(self, screen):
+        # py.draw.rect(screen, "#f8f6f9", self.rect)
+        screen.blit(self.img, (self.x, self.y))
+    
+    def move(self, screen, grid, event):
+        r = self.y // 60
+        c = self.x // 60
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_LEFT and c - 1 >= 0 and grid[r][c-1] != 0:
+                self.x -= 60
+                Player.dig.play()
+            if event.key == py.K_RIGHT and c + 1 < len(grid[0]) and grid[r][c+1] != 0:
+                self.x += 60
+                Player.dig.play()
+            if event.key == py.K_UP and r - 1 >= 0 and grid[r-1][c] != 0:
+                self.y -= 60
+                Player.dig.play()
+            if event.key == py.K_DOWN and r + 1 < len(grid) and grid[r+1][c] != 0:
+                self.y += 60
+                Player.dig.play()
+            '''
+            if event.key == py.K_SPACE and grid[r][c] == 2:
+                grid[r][c] = 1
+                Player.coin += 1
+            '''
+            
+        '''
+        keys = py.key.get_pressed() #checks of a key is pressed
+        if keys[py.K_a] and self.x > 0:
+            self.x -= Player.speedX
+        if keys[py.K_d] and self.x < 600 - self.w:
+            self.x += Player.speedX
+        if keys[py.K_w] and self.y > 0:
+            self.y -= Player.speedY
+        if keys[py.K_s] and self.y < 600 - self.h:
+            self.y += Player.speedY
+        #once the (x,y) are updated then we must update rect object
+        self.rect = (self.x, self.y, self.w, self.h)
+        '''
+        
+    def mine(self, screen, grid, event):
+        r = self.y // 60
+        c = self.x // 60
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_a and c - 1 >= 0 and grid[r][c-1] == 0:
+                grid[r][c-1] = 1
+                Player.dig.play()
+            elif event.key == py.K_d and c + 1 < len(grid[0]) and grid[r][c+1] == 0:
+                grid[r][c+1] = 1
+                Player.dig.play()
+            elif event.key == py.K_w and r - 1 >= 0 and grid[r-1][c] == 0:
+                grid[r-1][c] = 1
+                Player.dig.play()
+            elif event.key == py.K_s and r + 1 < len(grid) and grid[r+1][c] == 0:
+                grid[r+1][c] = 1
+                
+                Player.dig.play()
+        return grid
+
+    def collision(self,enemy):
+        #enemy is also a player object
+        #the collision happens if the difference between x cord and y cord is
+        #less than width and height respectively
+        if abs(self.x-enemy.x) <= self.w and abs(self.y - enemy.y) <= self.h:
+            if self.collide == False:#so if the collision happens first time
+                #then we print the collision
+                print("collision")
+                #then we set it's value to True
+                self.collide = True
+        elif self.collide == True:
+            self.collide = False
+                
+
+class Obstacle:
+    '''
+    This is an obstacle which will have fixed size for now
+    and we only need to store (x, y) coordinates.
+    '''
+
+    def __init__(self, x:int, y:int, img):
+        self.x = x
+        self.y = y
+        self.img = img
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.x, self.y))
+        # return py.draw.rect(screen, "#000000", (self.x, self.y, 60, 60))
+
+"""
+class Coin:
+    '''
+    This is an obstacle which will have fixed size for now
+    and we only need to store (x, y) coordinates.
+    '''
+
+    def __init__(self, x:int, y:int, img):
+        self.x = x
+        self.y = y
+        self.img = img
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.x, self.y))
+        # return py.draw.rect(screen, "#000000", (self.x, self.y, 60, 60))
+"""
